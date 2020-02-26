7e10926a70b6f563ef8d6e594e077765bc907483
Hampus-Martensson-Bondjers <hampus.martenssonbondjers@elev.ga.ntig.se>
2020-02-26 15:02:47 +0100

    Commit1
    Eftersom jag har haft problem med att pusha innan så är detta allt som jag gjort hitils.

---
+++ js/index.js
@@ -0,0 +1,11 @@
+    const nav = document.querySelector('.navlinks')
+    const burger = document.querySelector('.burger')
+
+function toggleMeny() {
+
+    nav.classList.toggle('navActive')
+    burger.classList.toggle('toggle')
+
+}
+
+    burger.addEventListener('click',toggleMeny)
